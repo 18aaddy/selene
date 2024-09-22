9e3c7e27debc07c192cfbaa77db12b27a49ea0e5
18aaddy <aaddyrocks123@gmail.com>
2024-09-22 18:22:56 +0530

    Initial commit

--- client/client.go
+++ client/client.go
@@ -1,2 +1,19 @@
 package client
 
+import (
+	"github.com/BlocSoc-iitr/selene/config"
+)
+
+type ClientBuilder struct {
+	Network *config.Network
+	ConsensusRpc *string
+	ExecutionRpc *string
+	Checkpoint *[32]byte
+	RpcBindIp *string
+	RpcPort *uint16
+	DataDir *string
+	Config *config.Config
+	Fallback *string
+	LoadExternalFallback bool
+	StrictCheckpointAge bool
+}
